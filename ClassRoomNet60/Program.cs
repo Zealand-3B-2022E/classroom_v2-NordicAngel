007a83ef3de91a840268c675a7439b60dc52d5a5
NordicAngel <78414012+NordicAngel@users.noreply.github.com>
2022-09-05 13:35:23 +0200

    Added students to classroom

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassRoomNet60;
+
+Random random = new Random();
+
+ClassRoom @class = new ClassRoom();
+
+@class.ClassName = "Zealand-3B-2022E";
+@class.SemesterStart = new DateTime(2022, 9, 1);
+
+@class.Students.Add(new Student("Hannibal", random.Next(1, 12), random.Next(1, 31)));
+@class.Students.Add(new Student("Patrik", random.Next(1, 12), random.Next(1, 31)));
+@class.Students.Add(new Student("Mikkel", random.Next(1, 12), random.Next(1, 31)));
+@class.Students.Add(new Student("Emil", random.Next(1, 12), random.Next(1, 31)));
