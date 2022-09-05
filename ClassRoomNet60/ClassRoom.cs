f94f96ceba902b0660974432a259328cc7efa83f
NordicAngel <78414012+NordicAngel@users.noreply.github.com>
2022-09-05 13:26:28 +0200

    Created ClassRomm class

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassRoomNet60
+{
+    internal class ClassRoom
+    {
+        public string ClassName { get; set; }
+        public List<Student> Students { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+        public ClassRoom()
+        {
+
+        }
+    }
+}
